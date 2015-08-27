5e7eb65a2b034cf414e716ef17a9096e758c5875
hilpitome <hilpitome@gmail.com>
2015-08-27 22:48:51 +0300

    added notice

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,14 +15,14 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
-
+    flash.notice = "Article '#{@article.title}' created!"
     redirect_to article_path(@article)
 
   end
   def update
   @article = Article.find(params[:id])
   @article.update(article_params)
-
+  flash.notice = "Article '#{@article.title}' Updated!"
   redirect_to article_path(@article)
   end
 
@@ -30,6 +30,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
 
     @article.destroy
+    flash.notice = "Article '#{@article.title}' deleted!"
     redirect_to articles_path
   end
 
